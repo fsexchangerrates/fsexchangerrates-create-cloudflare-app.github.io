2e1747594877a2cb4564adfbdf1598154e1017a0
fsexchangerrates <fs.suppermaster@gmail.com>
2020-05-22 10:09:11 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 // this import statement tells webpack to include styles.css in the build
 import css from "./styles.css"
 
@@ -5,7 +6,9 @@ function init() {
     if (!window.addEventListener) return // Check for IE9+
 
     let options = INSTALL_OPTIONS
+    let product = INSTALL_PRODUCT
     let element
+    let appElement
 
     // updateElement runs every time the options are updated.
     // Most of your code will end up inside this function.
@@ -15,6 +18,12 @@ function init() {
         // Set the app attribute to your app's dash-delimited alias.
         element.setAttribute("app", "fsexchangerrates-create-cloudfare-app")
         element.textContent = options.message
+
+        appElement = INSTALL.createElement(options.location, appElement)
+        appElement.setAttribute('app', 'product-example')
+        if (product && product.id === 'pro') {
+            // Pro plan exclusive features.
+        }
     }
 
     // INSTALL_SCOPE is an object that is used to handle option changes without refreshing the page.
@@ -24,6 +33,13 @@ function init() {
 
             updateElement()
         },
+    }
+
+    window.INSTALL_PRODUCT = {
+        setOptions(nextOptions) {
+            product = nextOptions
+            updateElement()
+        }
     }
 
     // This code ensures that the app doesn't run before the page is loaded.
