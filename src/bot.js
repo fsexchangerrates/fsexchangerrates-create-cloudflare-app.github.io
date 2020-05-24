880c2bf106691aae34094cb20b163e9c54ca4df9
fsexchangerrates <fs.suppermaster@gmail.com>
2020-05-25 00:17:26 +0700

    update

--- src/bot.js
+++ src/bot.js
@@ -1,64 +1,16 @@
+import * as line from '@line/bot-sdk';
+
 import * as express from 'express';
 
-import * as bottender from 'bottender';
-
-import * as line from 'messaging-api-line';
-
-import {
-    LineBot,
-    LineConnector,
-    LineContext,
-    LineEvent,
-    LineTypes,
-} from 'bottender';
-
-import { Verify } from 'crypto';
-
-import { Session } from 'inspector';
+const middleware = line.middleware();
 
 const { config } = require('./config');
 
-const client = new bottender.LineConnector({
-    channelSecret: config.channelSecret,
-    accessToken: config.channelAccessToken
-});
+const client = new line.Client(config);
 
-const event = new bottender.LineEvent;
+let port = process.env.PORT || 3000;
 
-const initialState = bottender.initializeServer(express.application);
-
-const context = new bottender.LineContext(client, event, Session, initialState);
-
-let port = process.env.PORT;
-
-const server = express()
-server.listen(port || 3000, () => {
-    console.log('listening on port: '
-        `${port}`)
+const router = express();
+router.listen(port, () => {
+    console.log('listening on port: ', `${port}`)
 })
-
-server.post('/callbacl', line.Line(config), (req, res) => {
-    if (req.body.destination) {
-        console.log('Destination User ID: ' + req.body.destination);
-    }
-    if (!Array.isArray(req.body.events)) {
-        console.error('Error', Error);
-    }
-
-    Promise.all(req.body.events.map(event => {
-        console.log('event', event);
-        return handleEvent(event);
-    }).then((res) => {
-        res.status(200).send('OK').end();
-    }).catch((err) => {
-        throw new console.Error(err);
-    }));
-});
-
-const linebot = new bottender.LineBot(config);
-
-const bot = new bottender.Bot(linebot);
-
-async function handleEvent(event, client, c) {
-
-}
